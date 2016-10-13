7dbd666a3a062ef1042e4c705e09b05e98a92601
toby <toby.xu@tpv-tech.com>
2016-10-13 16:42:34 +0800

    add actionEntry method

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,8 @@ use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
 
+use app\models\EntryForm; // add xum
+
 class SiteController extends Controller
 {
     /**
@@ -136,5 +138,19 @@ class SiteController extends Controller
     public function actionSayHello(){
     	$message = "hello world!";
     	return $this->render('say',['message'=>$message]);
+    }
+    
+    /**
+     * Verify the use information.
+     *
+     * @return string
+     */
+    public function actionEntry(){
+    	$model = new EntryForm();
+    	if($model->load(Yii::$app->request->post()) && $model->validate()){
+    		return $this->render("entry-confirm",["model"=>$model]);
+    	}else{
+    		return $this->render("entry",["model"=>$model]);
+    	}
     }
 }
