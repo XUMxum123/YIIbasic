b3b1c1baa21142bdebfe26d25b3f48b09bc00154
meng.xu <18850337241@163.com>
2016-10-13 21:29:53 +0800

    add news and newssearch by gii

---
+++ models/News.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property string $id
+ * @property string $title
+ * @property string $content
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'title', 'content'], 'required'],
+            [['id'], 'string', 'max' => 32],
+            [['title'], 'string', 'max' => 100],
+            [['content'], 'string', 'max' => 1000],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+        ];
+    }
+}
