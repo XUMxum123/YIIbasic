ad70a436157bbc8a27b4aa1c199f63fd44e5f530
xum <xum1281517532@163.com>
2016-11-14 16:24:00 +0800

    modify to test it

---
+++ views/channel/channel/widget.php
@@ -0,0 +1,18 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\URL;
+//use yii\dosamigos\DatePicker;
+?>
+
+<div>
+  <h1>Widget</h1><br />
+  <?php 
+/*   echo DatePicker::widget([
+  		'language' => 'zh-CN',
+  		'name' => 'country',
+  		'clientOptions' => [
+  				'dateFormat' => 'yy-mm-dd',
+  		],
+  ]); */
+  ?>
+</div>
