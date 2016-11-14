b9652ea9b9c8c0dae355cce582919675985845bb
meng.xu <18850337241@163.com>
2016-11-14 21:11:20 +0800

    add jquery-ui fie, in order to study yii2-date plugin

--- views/channel/channel/widget.php
+++ views/channel/channel/widget.php
@@ -1,18 +1,18 @@
 <?php
 use yii\helpers\Html;
 use yii\helpers\URL;
-//use yii\dosamigos\DatePicker;
+use yii\jui\DatePicker;
 ?>
 
 <div>
   <h1>Widget</h1><br />
   <?php 
-/*   echo DatePicker::widget([
+  echo DatePicker::widget([
   		'language' => 'zh-CN',
   		'name' => 'country',
   		'clientOptions' => [
   				'dateFormat' => 'yy-mm-dd',
   		],
-  ]); */
+  ]);
   ?>
 </div>
